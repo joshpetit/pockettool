6b36be8a345c85d3a9dadb2504c057dbbcd9ec86
joshpetit <joshuapetitma@yahoo.com>
2020-06-22 08:25:09 -0400

    implemented TextSend

---
+++ src/textswap/TextSwap.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import {firestore} from '../common/firebase';
+
+class TextSwap extends Component {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return(
+        <div>
+
+        </div>
+        )
+    }
+}
